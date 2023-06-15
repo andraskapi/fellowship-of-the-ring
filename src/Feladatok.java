4484ae15b43b666fc564d9973e8db7083c07a3fb
andra <andras_kapitany@hotmail.hu>
2023-06-15 20:02:53 +0200

    update

---
+++ src/Feladatok.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Feladatok {
+    public static void main(String[] args) {
+
+
+        int[] numbers = {1, 2, 7, 2, 3, 4, 10, 8, 9, 11, 5, 4, 3, 6, 5, 6, 5, 1, 8, 10, 9, 15};
+        Set<Integer> listOfNumbers = new HashSet<>();
+        for (int num : numbers) {
+            listOfNumbers.add(num);
+        }
+        System.out.println(listOfNumbers);
+        listOfNumbers.size();
+        System.out.println(listOfNumbers.size());
+
+
+        Set<Integer> evenNumbers = new HashSet<>();
+        Set<Integer> oddNumbers = new HashSet<>();
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenNumbers.add(num);
+            } else {
+                oddNumbers.add(num);
+            }
+        }
+        System.out.println(evenNumbers);
+        System.out.println(oddNumbers);
+        System.out.println("even qty: " + evenNumbers.size() + " odd qty: " + oddNumbers.size());
+
+        Random ran = new Random();
+
+        List<Integer> diceThrow = new ArrayList<>();
+
+        Map<Integer, Integer> countDice = new HashMap<>();
+
+        double sum = 0;
+        int count = 0;
+
+
+        for (int i = 0; i < 1000; i++) {
+            int dice = ran.nextInt(1, 7);
+            diceThrow.add(dice);
+            sum += dice;
+
+        }
+        System.out.println(diceThrow.size());
+        double avg;
+        avg = sum / diceThrow.size();
+        System.out.println(avg);
+        int countTwo = 0;
+
+        for (int i = 0; i < diceThrow.size(); i++) {
+            if (diceThrow.contains(1)){
+                countTwo++;
+            }
+
+        }
+        int countOne = countDthr(diceThrow,1);
+        System.out.println(countOne);
+
+
+
+
+    }
+
+    public static int countDthr(List<Integer> diceThrow, int number){
+        int count = 0;
+        for (int num : diceThrow){
+            if(num == number){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+}
---
+++ src/leirasFeladatok.md
@@ -0,0 +1,22 @@
+1. Adott az alábbi tömb:
+   int[] numbers = {1, 2, 7, 2, 3, 4, 10, 8, 9, 11, 5, 4, 3, 6, 5, 6, 5, 1, 8, 10, 9, 15};
+   Készíts Set-et ebből a tömbből úgy, hogy végigmész a tömbön, és egyesével hozzáadod a Set-hez a tömbben szereplő számokat.
+   Hány darab különböző számot tartalmaz a tömb? (A létrehozott Set alapján ez már könnyen megmondható.) Gondolkozz!
+   Miért nem jó a kérdés megválaszolására, ha List-et készítesz Set helyett?
+   És milyen sorrendben szerepelnek a számok a létrehozott Set-ben? (Írd ki a képernyőre, nézd meg!)
+
+2. Az előző feladat tömbjét használd!
+   Válogasd szét a tömb számait: külön legyenek a párosok, külön a páratlanok.
+   Ezúttal is használj Set-et!
+   Hány darab különböző páros és páratlan számot tartalmaz a tömb?
+
+3. Készíts egy dobókockát (random szám generálás 1-6 között), amivel dobj 1000-szer!
+   A dobás eredményeit List-ben tárold! Számítsd ki a dobások átlagát!
+   Írd ki minden számra, hányszor lett dobva! Pl. 1 -> 150 db dobás
+
+4. Adott az alábbi tömb:
+   int[] numbers = {34808, 422, 334, 12014, 412617, 292389, 342260, 32244, 334534, 497721, 3775, 544, 433, 55340, 257952, 1496173, 73, 539141, 3355, 305720, 3677429, 114047, 5656 };
+   Írj metódusokat az alábbi kérdések megválaszolására:
+   ⎼ Van ismétlődés a tömbben? (Szerepel-e többször ugyanaz a szám?)
+   ⎼ Hány számjegyből állnak a számok a tömbben? Vizsgáld meg, hogy a számok hány számjegyből állnak - másképp, hogy milyen “hosszúak”! Pl. a tömb első eleme, a 34808 5 számjegyből áll, azaz 5 “hosszú”, úgymond.
+   ⎼ Mennyi a legkisebb és a legnagyobb “hosszúság”?
